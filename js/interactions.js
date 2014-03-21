3022cbdc00703e403b29f017591130ba0e305f2c
Blanca Tortajada <tortajadablanca@gmail.com>
2014-03-21 11:15:16 +0000

    added syntax highlighting and code markup

---
+++ js/interactions.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('button#trigger-menu').click(function() {
+    $('ul#button-menu').slideToggle('slow');
+  });
+
+  $('button.data').click(function() {
+    $('ul#content-small').toggle('slow');
+    $('button.data').css({"background":"#4c9cda","color":"#fff", "border":"1px solid #4c9cda"});
+  });
+
+  $('button#fields-button').click(function() {
+    $('ul#fields-small').toggle('slow');
+    $('button#fields-button').css({"background":"#4c9cda","color":"#fff", "border":"1px solid #4c9cda"})
+  });
+
+  $('div.status#ok').slideDown('normal');
+  $('div.status#warning').slideDown('normal');
+
+  $('div.status#ok i.icon-chevron-up').click(function() {
+    $('div.status#ok').slideUp('normal');
+  });
+
+  $('div.status#warning i.icon-chevron-up').click(function() {
+    $('div.status#warning').slideUp('normal');
+  });
+
+  $('ul.subnav-small li.header#strengthen-button i').click(function() {
+    $('ul.expertise-small#strengthen').slideToggle('normal');
+  });
+
+  $('ul.subnav-small li.header#specialise-button i').click(function() {
+    $('ul.expertise-small#specialise').slideToggle('normal');
+  });
+
+});
